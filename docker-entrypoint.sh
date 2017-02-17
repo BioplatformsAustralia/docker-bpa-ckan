eb9532bcc6a1beeb41cf437b71491ac416f65772
Grahame Bowland <grahame@angrygoats.net>
2017-02-17 14:12:15 +0800

    skip index rebuild on restart

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -125,9 +125,11 @@ if [ "$1" = 'uwsgi' ]; then
     : ${UWSGI_OPTS="/etc/uwsgi/uwsgi.ini"}
     echo "UWSGI_OPTS is ${UWSGI_OPTS}"
 
-    # sync up with the SOLR server after container restart
-    echo "** rebuilding Solr index"
-    paster --plugin=ckan search-index rebuild -c /etc/ckan/default/ckan.ini
+    # due to the huge number of packages, this is too slow to run every startup
+    # 
+    # # sync up with the SOLR server after container restart
+    # echo "** rebuilding Solr index"
+    # paster --plugin=ckan search-index rebuild -c /etc/ckan/default/ckan.ini
 
     exec uwsgi --die-on-term --ini ${UWSGI_OPTS}
 fi
