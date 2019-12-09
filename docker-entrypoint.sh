c1bc98535510944f3d80976b30fcdeb3c7a7d11e
Grahame Bowland <grahame.bowland@qcif.edu.au>
2019-12-09 14:22:45 +0800

    fix SITE_URL

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -67,7 +67,7 @@ function defaults {
     : ${CKAN_DATASTORE_WRITE_URL="postgres://${DATASTORE_DBUSER}:${DATASTORE_DBPASS}@${DATASTORE_DBSERVER}/${DATASTORE_DBNAME}"}
     : ${CKAN_DATASTORE_READ_URL="postgres://${DATASTORE_DB_READONLY_USER}:${DATASTORE_DB_READONLY_PASS}@${DATASTORE_DBSERVER}/${DATASTORE_DBNAME}"}
     : ${CKAN_SOLR_URL="http://solr:8983/solr/ckan"}
-    : ${CKAN_SITE_URL:="https://localhost:8443/app/"}
+    : ${CKAN_SITE_URL:="https://localhost:8443/"}
     : ${GOOGLE_UA:="UA-UNSET"}
     : ${CKAN_STORAGE_PATH:='/var/www/storage/'}
     : ${CKAN_PREFIX:='/app'}
