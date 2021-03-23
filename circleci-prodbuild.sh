45b0e393abb69369cc0de9985bdb193a92ef5245
mattRedBox <matt@redboxresearchdata.com.au>
2021-03-23 15:33:00 +1000

    corrected naming.

--- circleci-prodbuild.sh
+++ circleci-prodbuild.sh
@@ -6,9 +6,9 @@ set -e
 # Production (deployable) build and tests
 #
 
-docker build -t bioplatformsaustralia/${GIT_REPO_NAME}:latest .
-docker push bioplatformsaustralia/${GIT_REPO_NAME}
+docker build -t bioplatformsaustralia/${DOCKER_NAME}:latest .
+docker push bioplatformsaustralia/${DOCKER_NAME}
 if [ x"$GIT_TAG" != x"" ]; then
-  docker tag bioplatformsaustralia/${GIT_REPO_NAME}:latest bioplatformsaustralia/${GIT_REPO_NAME}:${GIT_TAG}
-  docker push bioplatformsaustralia/${GIT_REPO_NAME}:${GIT_TAG}
+  docker tag bioplatformsaustralia/${DOCKER_NAME}:latest bioplatformsaustralia/${DOCKER_NAME}:${GIT_TAG}
+  docker push bioplatformsaustralia/${DOCKER_NAME}:${GIT_TAG}
 fi
